da2e22caf6b99741f20926f90bb13b133d224110
Kebabsavar31 <147258058+kebabistik@users.noreply.github.com>
2025-01-29 03:25:35 +0300

    Add files via upload

---
+++ Kalp Çizme.py
@@ -0,0 +1,15 @@
+import turtle as tr
+import time
+tr.pensize(2)
+tr.color("red")
+tr.speed(1)
+
+tr.begin_fill()
+tr.left(50)
+tr.forward(133)
+tr.circle(50,200)
+tr.right(140)
+tr.circle(50,200)
+tr.forward(133)
+tr.end_fill()
+time.sleep(2)
